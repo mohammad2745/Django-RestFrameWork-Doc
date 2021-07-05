d5fb633a33c48b7f8419f9eaec07e2749f4865e0
Mohammad Bin Sultan <mohammadfcc2745@gmail.com>
2021-07-05 14:55:58 +0600

    Serializers, Views updated

--- tutorial/settings.py
+++ tutorial/settings.py
@@ -122,6 +122,11 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 
+REST_FRAMEWORK = {
+    'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.PageNumberPagination',
+    'PAGE_SIZE': 10
+}
+
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
 
